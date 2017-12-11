9a565269e6a85d0b1d22d89ee3f22d597d7a7040
Yash Malik <yamalik@ucsd.edu>
2017-12-11 12:37:32 -0800

    fixed schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE axioms_db;
-USE burgers_db;
+USE axioms_db;
 CREATE TABLE axioms (
 	id int NOT NULL AUTO_INCREMENT,
-	axiom_text varchar(50),
+	axiom_text varchar(500),
 	claimed BOOLEAN,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY(id)
